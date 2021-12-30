0d2984ecd0ca1e8b2bc8a3a3974c229ae07f3a20
Yahya Khodr <yhy97kh18@gmail.com>
2021-12-30 21:11:32 +0200

    gitignore fixing

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,4 @@
 const String ARTICLE_RESPONSE =
     'http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=$apiKey';
 
-const String apiKey = 'eFnmzNHHfebKGgCz1NDFun90CzENYq6i';
+const String apiKey = 'Add Your API KEY HERE';
